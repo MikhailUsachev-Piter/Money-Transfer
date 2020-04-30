bbdf6ae7f25bcefcc2b8747e125b778e554a59b5
Mikhail Usachev <63136175+MikhailUsachev-Piter@users.noreply.github.com>
2020-04-30 15:15:27 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 2_147_483_640;
+        int transfer = 9;
+        int total = balance + transfer;
+        System.out.println(total);
+    }
+}
